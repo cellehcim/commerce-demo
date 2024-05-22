d41d5eeb08dab7aab5ca48cbb4bfaa8bbb3acb31
M. Mishieru <mchan324@my.bcit.ca>
2024-05-22 13:42:32 -0700

    cart details also accepts lowercase input

--- src/main/java/com/cellehcim/commercedemo/CartDetails.java
+++ src/main/java/com/cellehcim/commercedemo/CartDetails.java
@@ -7,7 +7,6 @@ import com.commercetools.api.models.cart.LineItemDraft;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class CartDetails {
@@ -15,13 +14,11 @@ public class CartDetails {
     @NotNull(message = "The country code must be included as a two-digit ISO 3166-1 alpha-2 country code")
     @NotBlank(message = "The country code must be included as a two-digit ISO 3166-1 alpha-2 country code")
     @Size(min = 2, max = 2, message = "The country code must be exactly two digits")
-    @Pattern(regexp = "^[A-Z]*$", message = "The country code must be uppercase")
     private String countryCode;
 
     @NotNull(message = "The currency code must be included as a three-digit ISO 4217 currency code")
     @NotBlank(message = "The currency code must be included as a three-digit ISO 4217 currency code")
     @Size(min = 3, max = 3, message = "The currency code must be exactly three-digits")
-    @Pattern(regexp = "^[A-Z]*$", message = "The currency code must be uppercase")
     private String currency;
 
     private String[] lineItemSkus;
@@ -29,8 +26,8 @@ public class CartDetails {
     private List<LineItemDraft> lineItems;
 
     public CartDetails(String countryCode, String currency, String[] lineItemSkus) {
-        this.countryCode = countryCode;
-        this.currency = currency;
+        this.countryCode = countryCode.toUpperCase();
+        this.currency = currency.toUpperCase();
         this.lineItems = createLineItems(lineItemSkus);
     }
 
@@ -76,7 +73,7 @@ public class CartDetails {
         List<LineItemDraft> lineItemArrayList = new ArrayList<LineItemDraft>();
 
         for (String sku : lineItemProductSkus) {
-            LineItemDraft lineItem = LineItemDraft.builder().sku(sku).build();
+            LineItemDraft lineItem = LineItemDraft.builder().sku(sku.toUpperCase()).build();
             lineItemArrayList.add(lineItem);
         }
 
