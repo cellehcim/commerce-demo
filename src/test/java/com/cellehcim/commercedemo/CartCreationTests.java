a5204a9339431cb513ff7bf6b4e62e227bc15fb2
M. Mishieru <mchan324@my.bcit.ca>
2024-05-27 12:44:09 -0700

    tests now consider line item quantities

--- src/test/java/com/cellehcim/commercedemo/CartCreationTests.java
+++ src/test/java/com/cellehcim/commercedemo/CartCreationTests.java
@@ -63,7 +63,7 @@ class CartCreationTests {
 		requestParams.put("countryCode", "US"); 
 
 		JSONArray lineItemsJson = new JSONArray();
-		requestParams.put("lineItemSkus", lineItemsJson);
+		requestParams.put("lineItemDetail", lineItemsJson);
 
 		request.header("Content-Type", "application/json");
 		request.body(requestParams.toString());
@@ -78,7 +78,7 @@ class CartCreationTests {
 	}
 
 	@Test
-	void createEmptyCartWithOneLineItem() throws Exception {
+	void createCartWithOneLineItem() throws Exception {
 
 		RestAssured.baseURI = TestConstants.ENDPOINT_URL; 
 		RequestSpecification request = RestAssured.given();
@@ -88,8 +88,12 @@ class CartCreationTests {
 		requestParams.put("countryCode", "US"); 
 
 		JSONArray lineItemsJson = new JSONArray();
-		lineItemsJson.put("ARG-56");
-		requestParams.put("lineItemSkus", lineItemsJson);
+		JSONObject lineItem = new JSONObject();
+		lineItem.put("sku", "ARG-56");
+		lineItem.put("quantity", (long) 1);
+		lineItemsJson.put(lineItem);
+
+		requestParams.put("lineItemDetails", lineItemsJson);
 
 		request.header("Content-Type", "application/json");
 		request.body(requestParams.toString());
@@ -106,7 +110,7 @@ class CartCreationTests {
 	}
 
 	@Test
-	void createEmptyCartWithMoreThanLineItem() throws Exception {
+	void createCartWithMoreThanLineItem() throws Exception {
 
 		RestAssured.baseURI = TestConstants.ENDPOINT_URL; 
 		RequestSpecification request = RestAssured.given();
@@ -117,9 +121,18 @@ class CartCreationTests {
 		requestParams.put("countryCode", "US"); 
 
 		JSONArray lineItemsJson = new JSONArray();
-		lineItemsJson.put("ARG-56");
-		lineItemsJson.put("ADCT-01");
-		requestParams.put("lineItemSkus", lineItemsJson);
+		
+		JSONObject lineItemOne = new JSONObject();
+		lineItemOne.put("sku", "ARG-56");
+		lineItemOne.put("quantity", (long) 1);
+		lineItemsJson.put(lineItemOne);
+
+		JSONObject lineItemTwo = new JSONObject();
+		lineItemTwo.put("sku", "ADCT-01");
+		lineItemTwo.put("quantity", (long) 1);
+		lineItemsJson.put(lineItemTwo);
+
+		requestParams.put("lineItemDetails", lineItemsJson);
 
 		request.header("Content-Type", "application/json");
 		request.body(requestParams.toString());
@@ -147,9 +160,12 @@ class CartCreationTests {
 		requestParams.put("countryCode", "US"); 
 
 		JSONArray lineItemsJson = new JSONArray();
-		lineItemsJson.put("arg-56");
-		lineItemsJson.put("ADCT");
-		requestParams.put("lineItemSkus", lineItemsJson);
+		JSONObject lineItem = new JSONObject();
+		lineItem.put("sku", "ARG-5");
+		lineItem.put("quantity", (long) 1);
+		lineItemsJson.put(lineItem);
+
+		requestParams.put("lineItemDetails", lineItemsJson);
 
 		request.header("Content-Type", "application/json");
 		request.body(requestParams.toString());
@@ -172,9 +188,17 @@ class CartCreationTests {
 		requestParams.put("countryCode", "us"); 
 
 		JSONArray lineItemsJson = new JSONArray();
-		lineItemsJson.put("arg-56");
-		lineItemsJson.put("adct-01");
-		requestParams.put("lineItemSkus", lineItemsJson);
+
+		JSONObject lineItemOne = new JSONObject();
+		lineItemOne.put("sku", "arg-56");
+		lineItemOne.put("quantity", (long) 1);
+		lineItemsJson.put(lineItemOne);
+
+		JSONObject lineItemTwo = new JSONObject();
+		lineItemTwo.put("sku", "adct-01");
+		lineItemTwo.put("quantity", (long) 1);
+		lineItemsJson.put(lineItemTwo);
+		requestParams.put("lineItemDetails", lineItemsJson);
 
 		request.header("Content-Type", "application/json");
 		request.body(requestParams.toString());
@@ -190,4 +214,55 @@ class CartCreationTests {
 		assertThat(bodyString).contains("Ashen Rug", "Coffee Table");
 	}
 
+	void createCartWithOneLineItemZeroQuantity() throws Exception {
+
+		RestAssured.baseURI = TestConstants.ENDPOINT_URL; 
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject requestParams = new JSONObject(); 
+		requestParams.put("currency", "USD"); 
+		requestParams.put("countryCode", "US"); 
+
+		JSONArray lineItemsJson = new JSONArray();
+		JSONObject lineItem = new JSONObject();
+		lineItem.put("sku", "ARG-56");
+		lineItem.put("quantity", (long) 0);
+		lineItemsJson.put(lineItem);
+
+		requestParams.put("lineItemDetails", lineItemsJson);
+
+		request.header("Content-Type", "application/json");
+		request.body(requestParams.toString());
+		
+		Response response = request.post(""); 
+		int statusCode = response.getStatusCode(); 
+
+		assertThat(statusCode).isEqualTo(404);
+	}
+
+	void createCartWithOneLineItemNegativeQuantity() throws Exception {
+
+		RestAssured.baseURI = TestConstants.ENDPOINT_URL; 
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject requestParams = new JSONObject(); 
+		requestParams.put("currency", "USD"); 
+		requestParams.put("countryCode", "US"); 
+
+		JSONArray lineItemsJson = new JSONArray();
+		JSONObject lineItem = new JSONObject();
+		lineItem.put("sku", "ARG-56");
+		lineItem.put("quantity", (long) -1);
+		lineItemsJson.put(lineItem);
+
+		requestParams.put("lineItemDetails", lineItemsJson);
+
+		request.header("Content-Type", "application/json");
+		request.body(requestParams.toString());
+		
+		Response response = request.post(""); 
+		int statusCode = response.getStatusCode(); 
+
+		assertThat(statusCode).isEqualTo(404);
+	}
 }
